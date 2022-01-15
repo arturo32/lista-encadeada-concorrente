536776c13881b96f5cd18760406d23c56deec5cc
Arturo Fonseca <arturofonsecas32@gmail.com>
2022-01-15 17:48:51 -0300

    Adiciona mensagens de aviso na ações sobre o mutex

--- src/lista_encadeada.cpp
+++ src/lista_encadeada.cpp
@@ -1,22 +1,31 @@
 #include "lista_encadeada.h"
 
 void lista_encadeada::leLista(){
+	mtx.lock();
+	std::cout << "Mutex bloqueado em leLista." << std::endl;
+
 	for(auto it{this->lista.begin()}; it != this->lista.end(); ++it){
 		std::cout << *it << " ";
 	}
 	std::cout << std::endl;
+
+	std::cout << "Mutex liberado em leLista." << std::endl;
+	mtx.unlock();
 }
 
 void lista_encadeada::removeElemento(int elemento){
 	mtx.lock();
+	std::cout << "Mutex bloqueado em removeElemento." << std::endl;
 
 	this->lista.remove(elemento);
 
+	std::cout << "Mutex liberado em removeElemento." << std::endl;
 	mtx.unlock();
 }
 
 void lista_encadeada::insereElemento(int elemento){
 	mtx.lock();
+	std::cout << "Mutex bloqueado em insereElemento." << std::endl;
 	
 	auto it = this->lista.before_begin();
 	auto it2 = it;
@@ -28,5 +37,6 @@ void lista_encadeada::insereElemento(int elemento){
 	}
 	this->lista.insert_after(it2, elemento);
 	
+	std::cout << "Mutex liberado em insereElemento." << std::endl;
 	mtx.unlock();
 }
